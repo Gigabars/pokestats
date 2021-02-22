8219b1b9d7045c93638244f3f2a6762f1b2412bd
Gigabars <thegameru2016@gmail.com>
2021-02-22 11:07:40 -0500

    Redesign
    Internal changes for a redesign.

---
+++ pageturn.js
@@ -0,0 +1,28 @@
+var page = 1;
+
+function pageturn()
+{
+//page = page + 1;
+switch(page) {
+//name
+case 1:
+//This should never happen. If it does well then I broke it.
+alert("Error! Something went wrong.");
+break;
+//typing
+case 2:
+
+
+
+
+break;
+// stats
+case 3:
+
+
+
+
+
+break;
+}
+}
